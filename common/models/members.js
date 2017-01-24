45a408d1c0f52e473a8410b2216c5aedb9001fa8
fuguh <fuguh@dataon.com>
2017-01-24 13:56:28 +0700

    update member

--- common/models/members.js
+++ common/models/members.js
@@ -2,4 +2,31 @@
 
 module.exports = function(Members) {
 
+
+     Members.remoteMethod(
+        'getRandomMembers', {
+            accepts: [{
+                arg: 'params',
+                type: 'Object',
+                required: true,
+                http: { source: 'body' }
+            }],
+            returns: {
+                arg: 'accessToken', type: 'object', root: true,
+            },
+            http: {
+                path: '/getRandomMembers',
+                verb: 'post'
+            }
+        });
+
+    Members.getRandomMembers = function (params, cb) {
+        // let modelmembers = app.models.Members;
+        // let ds=Members.data
+        // modelmembers.find(
+            
+        // )
+        console.log(params);
+    }
+
 };
