a9856893e63e6fe388f010c2972a554286069062
Imron Rosdiana <imron.rosdiana@dataon.com>
2017-02-09 19:17:39 +0700

    Add chat

---
+++ common/models/match-member.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function(Matchmember) {
+    // Matchmember.remoteMethod('createChat', {
+    //     http: { path: '/createChat', verb: 'post' },
+    //     accepts: { arg: 'param', type: 'Object' },
+    //     returns: { arg: 'response', type: 'array',  root: true }
+    // });
+
+    // Matchmember.createChat = function(data, cb) {
+    // 	var socket = Matchmember.app.io;
+
+    //     Matchmember.create(data, function (err, result) {
+    //     	if (err) {
+    //     		cb(err);
+    //     		return;
+    //     	}
+
+    //         socket.emit('chating', result);
+    //     	cb(null, result);
+    //     });
+    // };
+};
