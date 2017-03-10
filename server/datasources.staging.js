488ff0a7b652a333dc953ce01cd610538885171c
Imron Rosdiana <imron.rosdiana@dataon.com>
2017-03-10 16:19:11 +0700

    Add deploy for testing

---
+++ server/datasources.staging.js
@@ -0,0 +1,15 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = {
+  db: {
+    connector: 'mysql',
+    hostname: '192.168.0.100',
+    port: 3306,
+    user: 'flutterasia',
+    password: 'pmj4k4rt@',
+    database: 'pmjakarta',
+  }
+};
