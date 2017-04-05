0a7c38bb638f3889b3fce0eace11aa8f735f6ddb
Kelvindo Sutan <kelvindo@dataon.com>
2017-04-05 11:54:20 +0700

    add new common function untuk async loop.

---
+++ common/common-util.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+    asyncLoop: asyncLoop
+}
+
+function asyncLoop(iterations, func, callback) {
+    var index = 0;
+    var done = false;
+    var loop = {
+        next: function () {
+            if (done) {
+                return;
+            }
+
+            if (index < iterations) {
+                index++;
+                func(loop);
+
+            } else {
+                done = true;
+                callback();
+            }
+        },
+
+        iteration: function () {
+            return index - 1;
+        },
+
+        break: function () {
+            done = true;
+            callback();
+        }
+    };
+    loop.next();
+    return loop;
+}
