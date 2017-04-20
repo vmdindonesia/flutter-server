e2ee35586b2141b25b3e7f7110b1c9d459fafb81
Kelvindo Sutan <kelvindo@dataon.com>
2017-04-20 15:48:45 +0700

    New util, for validate params.

--- common/common-util.js
+++ common/common-util.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     asyncLoop: asyncLoop,
+    validateParams: validateParams,
     calculateAge: calculateAge
-
 }
 
 function asyncLoop(iterations, func, callback) {
@@ -36,6 +36,43 @@ function asyncLoop(iterations, func, callback) {
     };
     loop.next();
     return loop;
+}
+
+function validateParams(params, key, dataType) {
+
+    if (typeof params !== 'object') {
+        throw {
+            name: 'validators.parmeter.datatype.mismatch',
+            status: 500, // INTERNAL SERVER ERROR
+            message: 'expected first parameter to be object, got ' + typeof params
+        };
+    } else if (typeof key !== 'string') {
+        throw {
+            name: 'validators.parameter.datatype.mismatch',
+            status: 500, // INTERNAL SERVER ERROR
+            message: 'expected second parameter to be string, got ' + typeof key
+        };
+    } else if (typeof dataType !== 'string') {
+        throw {
+            name: 'validators.params.datatype.mismatch',
+            status: 500, // INTERNAL SERVER ERROR
+            message: 'expected parameter to be string, got ' + typeof dataType
+        };
+    } else if (typeof params[key] === 'undefined') {
+        throw {
+            name: 'validators.key.not.found',
+            status: 500,
+            message: 'expected "' + key + '" key in object'
+        };
+    } else if (typeof params[key] !== dataType) {
+        throw {
+            name: 'validators.mismatch.datatype',
+            status: 500,
+            message: 'Mismatch data type of "' + key + '", expected ' + dataType + ' got ' + typeof key
+        };
+    } else {
+        return 0;
+    }
 }
 
 function calculateAge(birthday) { // birthday is a date
