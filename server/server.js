438ada047f4b8a6fe1e3f8f60b0cf0bbc7f93d46
Imron Rosdiana <imron.rosdiana@dataon.com>
2017-06-07 15:08:39 +0700

    Hide socket.io

--- server/server.js
+++ server/server.js
@@ -13,14 +13,10 @@ app.set('views', path.join(__dirname, '../common/views'));
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname);
-
 app.start = function () {
   // start the web server
-  return app.listen(function () {
-    app.emit('started');
+  var server = app.listen(function () {
+    app.emit('started', server);
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
@@ -28,24 +24,33 @@ app.start = function () {
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
+  return server;
 };
 
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function (err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
 // start the server if `$ node server.js`
-if (require.main === module) {
-  // app.start();
-  app.io = require('socket.io')(app.start());
-  var redis = require('socket.io-redis');
-  // app.io.adapter(redis({ host: 'localhost', port: 6379 }));
+// if (require.main === module) {
+//   // app.start();
+//   app.io = require('socket.io')(app.start());
 
-  app.io.on('connection', function (socket) {
-    console.log('socket is connected');
+//   app.io.on('connection', function (socket) {
+//     console.log('socket is connected');
 
-    socket.on('disconnect', function () {
-      console.log('socket disconnected');
-    });
+//     socket.on('disconnect', function () {
+//       console.log('socket disconnected');
+//     });
 
-    // Realtime socket.io
-    require('./realtime')(socket);
-  });
+//     // Realtime socket.io
+    // require('./realtime')(socket);
+//   });
 
-}
+// }
