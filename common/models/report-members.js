3cf9dce6715acc24f2a92f37cc8f2a2c23037a0a
Jeje Abdul Rahman <jejeabdulrahman@gmail.com>
2017-08-16 11:15:56 +0700

    RM search report member

--- common/models/report-members.js
+++ common/models/report-members.js
@@ -12,11 +12,17 @@ module.exports = function (Reportmembers) {
         returns: { arg: 'result', type: 'object', root: true }
     });
 
+    Reportmembers.remoteMethod('search', {
+        http: { verb: 'post' },
+        accepts: { arg: 'search', type: 'string', required: true },
+        returns: { arg: 'result', type: 'object', root: true }
+    });
     // END REMOTE METHOD ====================================================================
 
     // BEGIN LIST OF FUNCTION ===============================================================
 
     Reportmembers.getReportList = getReportList;
+    Reportmembers.search = search;
 
     // END LIST OF FUNCTION =================================================================
 
@@ -53,6 +59,52 @@ module.exports = function (Reportmembers) {
             }
 
             cb(null, result);
+        });
+    }
+
+    function search(text, cb) {
+
+        var filter = {
+            include: [
+                {
+                    relation: 'memberPhotos',
+                    scope: {
+                        fields: ['src']
+                    }
+                },
+                {
+                    relation: 'reportMember',
+                    scope: {
+                        where: {
+                            fullName: { like: '%' + text + '%' }
+                            // fullName: { like: '%Feri%' }
+                        }
+                        , fields: ['fullName']
+                    }
+                },
+                {
+                    relation: 'reportedBy',
+                    scope: {
+                        fields: ['fullName']
+                    }
+                }
+            ], order: 'createdDate DESC'
+        };
+
+        Reportmembers.find(filter, function (error, result) {
+            if (error) {
+                return cb(error);
+            }
+            var reportList = [];
+
+            result.forEach(function (item) {
+                item = JSON.parse(JSON.stringify(item));
+                if ('reportMember' in item) {
+                    reportList.push(item);
+                }
+            }, this);
+
+            cb(null, reportList);
         });
     }
 };
