dbd573b3773119b8e85a720d459ef46a8ae963b8
Kelvindo Sutan <kelvindosutan@gmail.com>
2017-08-30 14:58:49 +0700

    Modify Admin Verify

--- common/models/admin-verify.js
+++ common/models/admin-verify.js
@@ -2,9 +2,15 @@
 
 module.exports = function (Adminverify) {
 
+    var app = require('../../server/server');
+    var lodash = require('lodash');
+
     Adminverify.remoteMethod('getList', {
         http: { verb: 'get' },
         accepts: [
+            { arg: 'flag', type: 'number', required: true },
+            { arg: 'limit', type: 'number', required: true },
+            { arg: 'offset', type: 'number', required: true },
             { arg: 'options', type: 'object', http: 'optionsFromRequest' }
         ],
         returns: { arg: 'result', type: 'object', root: true }
@@ -12,18 +18,59 @@ module.exports = function (Adminverify) {
 
     Adminverify.getList = getList;
 
-    function getList(options, cb) {
+    function getList(flag, limit, offset, options, cb) {
 
-        var filter = {};
+        var Memberverifystatus = app.models.MemberVerifyStatus;
 
-        return Adminverify.find(filter, function (error, result) {
+        var orList = [];
+        var verifyList = ['ktp', 'sim', 'schoolCertificate', 'passport', 'businessCard'];
+
+        orList = verifyList.map(function (x) {
+            var temp = {};
+            temp[x] = flag;
+            return temp;
+        });
+
+        var filter = {
+            fields: [
+                'userId',
+                'ktp',
+                'sim',
+                'schoolCertificate',
+                'passport',
+                'businessCard',
+                'updateAt',
+                'createAt'
+            ],
+            where: {
+                or: orList
+            },
+            include: [{
+                relation: 'memberPhotos',
+                scope: {
+                    fields: [
+                        'ktp',
+                        'sim',
+                        'schoolCertificate',
+                        'passport',
+                        'businessCard'
+                    ]
+                }
+            }]
+        }
+
+        return Memberverifystatus.find(filter, function (error, result) {
             if (error) {
                 return cb(error);
             }
+
+
             return cb(null, result);
         });
 
 
     }
+
+
 
 };
